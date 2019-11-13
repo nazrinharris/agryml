c1309a4d1cf826c9c664e31db1b9eeb076983d1c
nazrinharris <totallyofficialharris@gmail.com>
2019-11-13 15:51:57 +0800

    Initial Commit

---
+++ firebase_auth/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\OMEN\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\FlutterProjects\mlkit-custom-image-classifier-master\firebase_auth"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\OMEN\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=0.8.4"
+export "FLUTTER_BUILD_NUMBER=5"
